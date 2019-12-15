fbdbb28613d5172a8782d0268ca0b7bbb316f1a4
Ian Luites <IanLuites@users.noreply.github.com>
2019-12-15 13:47:38 +0100

    Add benchmarks.

--- bench/iso.exs
+++ bench/iso.exs
@@ -19,3 +19,21 @@ Benchee.run(%{
   "NaiveDateTime" => fn -> NaiveDateTime.to_iso8601(naive_datetime) end,
   "UTCDateTime" => fn -> UTCDateTime.to_rfc3339(utc_datetime) end
 })
+
+Benchee.run(
+  %{
+    "DateTime" => &DateTime.from_iso8601/1,
+    "NaiveDateTime" => &NaiveDateTime.from_iso8601/1,
+    "UTCDateTime" => &UTCDateTime.from_iso8601/1
+  },
+  inputs: %{
+    "2019-01-01t23:00:01" => "2019-01-01T23:00:01",
+    "2019-01-01t23:00:01-07:00" => "2019-01-01T23:00:01-07:00"
+  }
+)
+
+Benchee.run(%{
+  "DateTime" => fn -> DateTime.to_iso8601(datetime) end,
+  "NaiveDateTime" => fn -> NaiveDateTime.to_iso8601(naive_datetime) end,
+  "UTCDateTime" => fn -> UTCDateTime.to_iso8601(utc_datetime) end
+})
